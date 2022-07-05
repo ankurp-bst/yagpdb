59bf952da8dc64936248c94475fe72872b2c74bc
pankajkumar-bst <pankajkumar.jaiswal@bluestacks.com>
2022-07-05 12:00:54 +0530

    added comment

--- .jenkins/execute.sh
+++ .jenkins/execute.sh
@@ -2,14 +2,13 @@ set -ex
 echo $(go env)
 echo $(which go)
 chmod -R 777 $(pwd)
-
 pwd=$(pwd)
 mkdir -p ${pwd}/bin/
 export GOBIN=${pwd}/bin/
 export GOCACHE="/tmp/" && cd ${pwd}/cmd/yagpdb && go install -ldflags "-X github.com/botlabs-gg/yagpdb/v2/common.VERSION=$(git describe --tags)"
 #export GOCACHE="/tmp/" && cd ${pwd}/cmd/dso-cli && go install -ldflags "-X github.com/botlabs-gg/yagpdb/v2/common.VERSION=$(git describe --tags)"
 cd ${pwd}
-VER=$(cat go.mod | egrep -m 1 github.com/botlabs-gg/yagpdb/v2)
+#VER=$(cat go.mod | egrep -m 1 github.com/botlabs-gg/yagpdb/v2)
 VER=`echo $VER | sed -e 's/^[[:space:]]*//'`
 VER=`echo ${VER} |sed -e 's/ /@/g'`
 echo $VER
