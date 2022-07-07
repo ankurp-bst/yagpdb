7cf1f21ef6f01229de96ec44aecb168020d1d21f
Ankur P <98815360+ankurp-bst@users.noreply.github.com>
2022-07-07 18:44:35 +0530

    removed tags

--- cmd/yagpdb/build.sh
+++ cmd/yagpdb/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VERSION=$(git describe --tags)
+VERSION=$(git describe)
 echo Building version $VERSION
 go build -ldflags "-X github.com/botlabs-gg/yagpdb/v2/common.VERSION=${VERSION}"
